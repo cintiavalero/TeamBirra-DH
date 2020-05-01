dc266146c522e15bb60dc4331692b4027709130b
cintiavalero <cintiavalero2001@gmail.com>
2020-05-01 11:15:29 -0300

    Proyecto en laravel, primer intento

---
+++ teamBirra1 - Laravel/resources/views/perfil.blade.php
@@ -0,0 +1,20 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+	<?@extends('head'); ?>
+  	<title>Team Birra | Perfil</title>
+  </head>
+  <body>
+@section('content')
+    @extends('footer')
+		@extends('perfilContext');
+  @endsection
+  @extends('layouts.app')
+		</body>
+
+  </body>
+</html>
